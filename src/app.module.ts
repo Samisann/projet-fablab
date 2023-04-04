8ca67f83c933fd0bec013122e8f86fd619872847
Olivier Assiene <olivier237assiene@gmail.com>
2023-04-04 10:21:12 +0200

    feat(reset_password

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/socialnetworks'),
     UserModule
   ],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}

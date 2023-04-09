f5849d8b9d741ef0e2653389559930edf0f4c981
Olivier Assiene <olivier237assiene@gmail.com>
2023-04-09 22:41:17 +0200

    feat(reset_password5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { AuthController } from './auth/auth.controller';
+import { EventController } from './event/controller/event.controller';
 import { PasswordResetService } from './auth/service/password-reset.service';
+import { AuthController } from './auth/auth.controller';
+import { UserService } from './user/service/UserService';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/socialnetworks'),
     UserModule
   ],
-  controllers: [AppController, AuthController],
-  providers: [AppService, PasswordResetService],
+  controllers: [AppController, EventController, AuthController],
+  providers: [AppService, UserService, PasswordResetService], 
 })
 export class AppModule {}

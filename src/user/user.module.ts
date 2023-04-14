0b3ad5010c42a38f0708536af908592765e3f64b
Olivier Assiene <olivier237assiene@gmail.com>
2023-04-14 18:49:46 +0200

    feat(Password forgot and updated in the database)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.model';
 import { UserController } from './controller/user.controller';
 import { UserService } from './service/UserService';
+<<<<<<< HEAD
 import { EventController } from './controller/event/event.controller';
+=======
+import { PasswordResetService } from './service/password-reset.service';
+>>>>>>> 1351ea5 (feat(Password forgot and updated in the database))
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
-    providers:[UserService],
+    providers:[UserService, PasswordResetService],
     controllers: [UserController],
 })
 export class UserModule {}

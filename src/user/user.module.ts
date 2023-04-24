948a35c576ee550cb9ae10d27cef85cac7f612d1
André Kouamé <kouameandre43@gmail.com>
2023-04-24 17:08:30 +0200

    chore(add_event_controller):add event controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,15 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.model';
 import { UserController } from './controller/user.controller';
 import { UserService } from './service/UserService';
-<<<<<<< HEAD
 import { EventController } from './controller/event/event.controller';
-=======
 import { PasswordResetService } from './service/password-reset.service';
->>>>>>> 1351ea5 (feat(Password forgot and updated in the database))
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
     providers:[UserService, PasswordResetService],
-    controllers: [UserController],
+    controllers: [UserController,EventController],
 })
 export class UserModule {}

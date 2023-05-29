60e09e5028c7971cf5c7f9a3d9767a9f604d1b89
Sami <sami-la98@hotmail.fr>
2023-05-29 15:26:12 -0400

    update

--- src/user/entities/user.model.ts
+++ src/user/entities/user.model.ts
@@ -18,7 +18,7 @@ export class User{
     @Prop({ required: true })
     createdAt: Date;
 
-    @Prop({required:true, unique:true})
+    @Prop({required:false, unique:true}) // remettre required true après test
     hobbies:{
         id:string;
         label:string;
